e58181167b1211593bd9c355f1c9f2f0aa44c16d
aafaq-cloud <Aafaqsikandar@gmail.com>
2023-03-11 21:53:27 +0500

    explore transition configuration in react, different states in react transition - entering, entered, exiting, exited states, in prop decides whether the transition component should be shown or not based on boolean value, mountOnEnter and unmountOnExit promise should add or remove transition element from the dom, timeout prop decides how long it take to switch transition element from entering to entered and from exiting to exited, pass arrow function to conditionally add css styles based on state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// import Transition from 'react-transition-group/Transition';
+
+import { Transition } from 'react-transition-group';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -8,6 +11,7 @@ import List from './components/List/List';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => {
@@ -22,6 +26,57 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() =>
+            this.setState(prevState => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <br />
+        {/* Now we use this transition component to wrap what we want to animate, in our case that's this div here. */}
+        {/* {this.state.showBlock ? (
+          <div
+            style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+            }}
+          ></div>
+        ) : null} */}
+        {/* in={inProp} This decides whether the elements wrapped inside the transition component should be shown or not. */}
+        {/* timeout it simply determines how long it takes to switch from entering to entered and from exiting to exited and it's this in-between thing you can then animate and that will be interpreted as milliseconds. */}
+        {/* We can add mount on enter which simply is a boolean prop which says yeah if the in prop is set to true you should add the wrap element to the dom and on the other end, you can add unmount on exit to really remove it from the dom if that's not true.  */}
+        <Transition
+          mountOnEnter
+          unmountOnExit
+          in={this.state.showBlock}
+          timeout={1000}
+        >
+          {/* {state => <p>{state}</p>} */}
+          {/* <p>{this.state.showBlock}</p> */}
+          {/* This arrow function should then of course return the jsx we actually want to render */}
+          {/* Now we know that state is entering, entered, exiting or exited, we can of course take advantage of this to now manipulate our style depending on the state. */}
+          {state => (
+            <div>
+              <p>State</p>
+              <p>{state}</p>
+              <div
+                style={{
+                  backgroundColor: 'red',
+                  width: 100,
+                  height: 100,
+                  margin: 'auto',
+                  transition: 'opacity 1s ease-out',
+                  opacity: state === 'exiting' ? 0 : 1,
+                }}
+              ></div>
+            </div>
+          )}
+        </Transition>
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         ) : null}
