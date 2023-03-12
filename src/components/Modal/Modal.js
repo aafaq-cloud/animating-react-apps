daa6506e1ebb66df7b22327205098fb804f48084
aafaq-cloud <Aafaqsikandar@gmail.com>
2023-03-12 08:57:02 +0500

    add our own classes in css transition component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,28 +5,6 @@ import { CSSTransition } from 'react-transition-group';
 import './Modal.css';
 
 const modal = props => {
-  //   const cssClasses = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed'];
-
-  // const cssClasses = [
-  //   'Modal',
-  //   props.show === 'entering'
-  //     ? 'ModalOpen'
-  //     : props.show === 'exiting'
-  //     ? 'ModalClosed'
-  //     : null,
-  // ];
-
-  // const cssClasses = ['Modal'];
-
-  // return (
-  //   <div className={cssClasses.join(' ')}>
-  //     <h1>A Modal</h1>
-  //     <button className="Button" onClick={props.closed}>
-  //       Dismiss
-  //     </button>
-  //   </div>
-  // );
-
   // ANIMATION TIMINGS
   const animationTiming = {
     enter: 400,
@@ -34,40 +12,17 @@ const modal = props => {
   };
 
   return (
-    // <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
-    // <Transition
-    //   mountOnEnter
-    //   unmountOnExit
-    //   in={props.show}
-    //   timeout={animationTiming}
-    // >
-    //   {state => {
-    //     const cssClasses = [
-    //       'Modal',
-    //       state === 'entering'
-    //         ? 'ModalOpen'
-    //         : state === 'exiting'
-    //         ? 'ModalClosed'
-    //         : null,
-    //     ];
-
-    //     return (
-    //       <div className={cssClasses.join(' ')}>
-    //         <h1>A Modal</h1>
-    //         <button className="Button" onClick={props.closed}>
-    //           Dismiss
-    //         </button>
-    //       </div>
-    //     );
-    //   }}
-    // </Transition>
-
     <CSSTransition
       mountOnEnter
       unmountOnExit
       in={props.show}
       timeout={animationTiming}
-      classNames="fade-slide"
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed',
+      }}
     >
       {/* The classes it cycles through are trunk */}
       {/* fade-slide-enter
